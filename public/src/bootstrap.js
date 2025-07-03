62ad9352259e3b21e28f40c47108a82c6bec2ef7
Patric Eberle <line-in@derverstaerker.ch>
2025-07-03 10:21:34 +0200

    Adds chat injection script

---
+++ public/src/bootstrap.js
@@ -0,0 +1,9 @@
+/**
+ * 1. ☑️ Add to DOM, add console.log()
+ * 2. Inject iFrame as last element on page
+ * 3. What about route updates in Vue?
+ */
+
+window.addEventListener('load',function () {
+  console.info('loaded');
+})
